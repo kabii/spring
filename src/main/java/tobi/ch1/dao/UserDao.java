cdd4ec134b98a5b2641072d105caea9b76151891
kabii <lovekabi@gmail.com>
2017-07-18 11:10:53 +0900

    Extract getConnection method

--- src/main/java/tobi/ch1/dao/UserDao.java
+++ src/main/java/tobi/ch1/dao/UserDao.java
@@ -6,8 +6,7 @@ import java.sql.*;
 
 public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/users", "root", "root");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) VALUES(?, ?, ?)");
 		ps.setLong(1, user.getId());
@@ -20,8 +19,7 @@ public class UserDao {
 	}
 
 	public User get(Long id) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/users", "root", "root");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setLong(1, id);
@@ -38,6 +36,12 @@ public class UserDao {
 		c.close();
 
 		return user;
+	}
+
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/users", "root", "root");
+		return c;
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
