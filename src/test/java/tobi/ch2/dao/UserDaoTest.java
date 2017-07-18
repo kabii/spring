ec84c66907043bd55e0d0947f531b4059420faaa
kabii <lovekabi@gmail.com>
2017-07-19 02:04:20 +0900

    Introduce JUnitTest

--- src/test/java/tobi/ch2/dao/UserDaoTest.java
+++ src/test/java/tobi/ch2/dao/UserDaoTest.java
@@ -1,18 +1,21 @@
 package tobi.ch2.dao;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import tobi.ch1.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws SQLException {
+	@Test
+	public void addAndGet() throws SQLException {
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		UserDao dao = context.getBean("userDao", UserDao.class);
-
 		User user = new User();
 		user.setId(1L);
 		user.setName("Nani");
@@ -23,12 +26,11 @@ public class UserDaoTest {
 		System.out.println(user.getId() + " registration success");
 
 		User user2 = dao.get(user.getId());
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("Test Fail (name)");
-		} else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("Test Fail (password");
-		} else {
-			System.out.println(user.getId() + " view success");
-		}
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+
+	public static void main(String[] args) {
+		JUnitCore.main("tobi.ch2.dao.UserDaoTest");
 	}
 }
