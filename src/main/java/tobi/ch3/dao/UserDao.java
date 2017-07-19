b2e906400dbad3732228ef44255d0e0f5b2f67df
kabii <lovekabi@gmail.com>
2017-07-20 00:51:21 +0900

    Prepare chapter 3

---
+++ src/main/java/tobi/ch3/dao/UserDao.java
@@ -0,0 +1,151 @@
+package tobi.ch3.dao;
+
+import lombok.Setter;
+import org.springframework.dao.EmptyResultDataAccessException;
+import tobi.ch2.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+	@Setter
+	private DataSource dataSource;
+
+	public void add(User user) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("insert into users(id, name, password) VALUES(?, ?, ?)");
+			ps.setLong(1, user.getId());
+			ps.setString(2, user.getName());
+			ps.setString(3, user.getPassword());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		ps.close();
+		c.close();
+	}
+
+	public User get(Long id) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			c = dataSource.getConnection();
+
+			ps = c.prepareStatement("select * from users where id = ?");
+			ps.setLong(1, id);
+
+			rs = ps.executeQuery();
+
+			User user = null;
+			if (rs.next()) {
+				user = new User(rs.getLong("id"), rs.getString("name"), rs.getString("password"));
+			}
+			if (user == null) {
+				throw new EmptyResultDataAccessException(1);
+			}
+			return user;
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public void deleteAll() throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public int getCount() throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+
+			rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+}
---
+++ src/test/java/tobi/ch3/dao/UserDaoTest.java
@@ -0,0 +1,78 @@
+package tobi.ch3.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tobi.ch2.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DaoFactoryTest.class)
+public class UserDaoTest {
+	@Autowired
+	private UserDao dao;
+
+	private User user1;
+	private User user2;
+	private User user3;
+
+	@Before
+	public void setUp() {
+		user1 = new User(1L, "Nani", "nani");
+		user2 = new User(2L, "Noel", "noel");
+		user3 = new User(3L, "Genji", "genji");
+		DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/users", "root", "root", true);
+		dao.setDataSource(dataSource);
+	}
+
+	@Test
+	public void addAndGet() throws SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+
+		User u1 = dao.get(user1.getId());
+		assertThat(u1.getName(), is(user1.getName()));
+		assertThat(u1.getPassword(), is(user1.getPassword()));
+
+		User u2 = dao.get(user2.getId());
+		assertThat(u2.getName(), is(user2.getName()));
+		assertThat(u2.getPassword(), is(user2.getPassword()));
+	}
+
+	@Test
+	public void count() throws SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+	}
+
+	@Test(expected = EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException {
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		dao.get(-1L);
+	}
+}
