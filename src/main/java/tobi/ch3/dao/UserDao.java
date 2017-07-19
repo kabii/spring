5875bd792ad9a16e718108f5595453f88911c2b8
kabii <lovekabi@gmail.com>
2017-07-20 01:03:57 +0900

    Introduce StatementStrategy and extract jdbcContextWithStatementStrategy method

--- src/main/java/tobi/ch3/dao/UserDao.java
+++ src/main/java/tobi/ch3/dao/UserDao.java
@@ -90,28 +90,7 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("delete from users");
-			ps.executeUpdate();
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+		jdbcContextWithStatementStrategy(new DeleteAllStatement());
 	}
 
 	public int getCount() throws SQLException {
@@ -143,6 +122,31 @@ public class UserDao {
 			if (rs != null) {
 				try {
 					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			ps = stmt.makePreparedStatement(c);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
 				} catch (SQLException e) {
 				}
 			}
