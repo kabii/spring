41f6c360b9949113f791b591cb10e4297a345c5a
kabii <lovekabi@gmail.com>
2017-07-19 17:49:36 +0900

    Test JUnitTest

---
+++ src/test/java/tobi/ch2/dao/JUnitTest.java
@@ -0,0 +1,53 @@
+package tobi.ch2.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/junit.xml")
+public class JUnitTest {
+	@Autowired
+	ApplicationContext context;
+
+	static Set<JUnitTest> testObjects = new HashSet<>();
+	static ApplicationContext contextObject = null;
+
+	@Test
+	public void test1() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+	}
+
+	@Test
+	public void test2() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+	}
+
+	@Test
+	public void test3() {
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+
+		assertThat(contextObject == null || contextObject == this.context, is(true));
+		contextObject = this.context;
+	}
+}
